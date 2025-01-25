da1d411faaaf62991be78c304182537d3b6be09d
eva picot <e.picot@alumni.alueducation.com>
2025-01-25 11:13:44 +0400

    viri-new

--- app/models/virus.rb
+++ app/models/virus.rb
@@ -2,4 +2,7 @@ class Virus < ApplicationRecord
   has_many :bookings
   has_many :reviews
   belongs_to :user
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, numericality: { greater_than: 0 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :viri, only: [:index, :new, :create]
-
+  resources :viri, only: [:new, :create, :show]
 end
